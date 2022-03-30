dbe93a016b4ea7ebfb005d74e36fd04f39ef109b
alamin <alaminripon5654@gmail.com>
2022-03-30 10:18:08 +0600

    User Chatbox Completed

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from http import server
+from concurrent.futures import thread
+from email import message
+from http import client, server
 import threading
 import socket
 
@@ -19,4 +21,34 @@ def broadcast(message):
 
 def handle(client):
   while True:
+    try:
+      message = client.recv(1024)
+      broadcast(message)
+    except:
+      index = clients.index(client)
+      clients.remove(client)
+      client.close()
+      nickname = nicknames[index]
+      broadcast(f'{nickname} left the chat'.encode('ascii'))
+      nicknames.remove(nickname)
+      break
     
+def receive():
+  while True:
+    client, address = server.accept()
+    print(f'connected with {str(address)}')
+
+    client.send('NICK'.encode('ascii'))
+    nickname = client.recv(1024).decode('ascii')
+    nicknames.append(nickname)
+    clients.append(client)
+
+    print(f'Nickname of the client is {nickname}!')
+    broadcast(f'{nickname} joined the chat!'.encode('ascii'))
+    client.send('Connected to the server!'.encode('ascii'))
+
+    thread = threading.Thread(target=handle, args=(client,))
+    thread.start()
+
+print("Server is listening......")
+receive()
